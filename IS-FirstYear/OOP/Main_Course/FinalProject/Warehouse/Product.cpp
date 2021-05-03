44796a2404d6dd75163ec795cd94cbcd76fbfb2a
Georgi Krastenov <gkrastenov@gmail.com>
2021-05-03 13:34:51 +0300

    add logic for product class

--- IS-FirstYear/OOP/Main_Course/FinalProject/Warehouse/Product.cpp
+++ IS-FirstYear/OOP/Main_Course/FinalProject/Warehouse/Product.cpp
@@ -1,1 +1,90 @@
 #include "Product.h"
+#include<cstring>
+
+Product::Product()
+{
+    description = nullptr;
+    expiryDate = nullptr;
+    entryDate = nullptr;
+    manufacturer = nullptr;
+    quantity = 0;
+    location = 0;
+    comment = nullptr;
+}
+
+Product::~Product()
+{
+    delete[] description;
+    delete[] manufacturer;
+    delete[] comment;
+
+    description = nullptr;
+    expiryDate = nullptr;
+    entryDate = nullptr;
+    manufacturer = nullptr;
+    quantity = 0;
+    location = 0;
+    comment = nullptr;
+}
+
+void Product::setDescription(const char* description) {
+    size_t lengthDescription = strlen(description) + 1;
+    this->description = new char[lengthDescription];
+    strcpy_s(this->description, lengthDescription, description);
+}
+
+void Product::setManufacturer(const char* manufacturer) {
+    size_t lengthManufacturer = strlen(manufacturer) + 1;
+    this->manufacturer = new char[lengthManufacturer];
+    strcpy_s(this->manufacturer, lengthManufacturer, manufacturer);
+}
+
+void Product::setComment(const char* comment) {
+    size_t lengthComment = strlen(comment) + 1;
+    this->comment = new char[lengthComment];
+    strcpy_s(this->comment, lengthComment, comment);
+}
+
+void Product::setQuantity(const int quantity) {
+    this->quantity = quantity;
+}
+
+void Product::setLocation(const int location) {
+    this->location = location;
+}
+
+void Product::setExpiryDate(const char* expiryDate) {
+    this->expiryDate = DateTime(expiryDate);
+}
+
+void Product::setEntryDate(const char* entryDate) {
+    this->expiryDate = DateTime(entryDate);
+}
+
+char* Product::getDescription() {
+    return this->description;
+}
+
+char* Product::getManufacturer() {
+    return this->manufacturer;
+}
+
+void Product::getExpiryDate() {
+    this->expiryDate.toString();
+}
+
+void Product::getEntryDate() {
+    this->entryDate.toString();
+}
+
+int Product::getQuantity() {
+    return this->quantity;
+}
+
+int Product::getLocation() {
+    return this->location;
+}
+
+char* Product::getComment() {
+    return this->comment;
+}
--- IS-FirstYear/OOP/Main_Course/FinalProject/Warehouse/Product.h
+++ IS-FirstYear/OOP/Main_Course/FinalProject/Warehouse/Product.h
@@ -18,4 +18,29 @@ private:
 	int quantity;
 	int location;
 	char* comment;
+
+public:
+	const int MAX_DESCRIPTION_LENGTH = 150;
+	const int MAX_MANUFACTURER_LENGTH = 50;
+	const int MAX_COMMENT_LENGTH = 300;
+
+	Product();
+	~Product();
+
+	void setDescription(const char* description);
+	void setManufacturer(const char* manufacturer);
+	void setExpiryDate(const char* expiryDate);
+	void setEntryDate(const char* entryDate);
+	void setQuantity(const int quantity);
+	void setLocation(const int location);
+	void setComment(const char* comment);
+	
+
+	char* getDescription();
+	char* getManufacturer();
+	void getExpiryDate();
+	void getEntryDate();
+	int getQuantity();
+	int getLocation();
+	char* getComment();
 };
